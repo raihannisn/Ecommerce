f31992ca1b371f2c167ecf1d02b942ba729ab064
raihannisn <raihannurinsan@gmail.com>
2024-08-03 21:55:38 +0700

    Update

---
+++ resources/views/admin/product/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('admin.layouts.app')
+@section('style')
+@endsection
+@section('content')
+<div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1>Edit Product</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary">
+                        <form action="" method="post">
+                            {{ csrf_field() }}
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label>Title<span style="color:red">*</span></label>
+                                    <input type="text" class="form-control" name="title" required value="{{ old('title', $product->title) }}" placeholder="Title">
+                                </div>                                
+                            </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="button" class="btn btn-danger float-right" onclick="window.location='{{ url('admin/category/list') }}'">Back</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+@endsection
+
+@section('script')
+<script src="{{ url('public/assets/dist/js/pages/dashboard3.js') }}"></script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\Admin\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,6 +49,11 @@ Route::group(['middleware' => 'admin'], function () {
     Route::post('admin/sub_category/edit/{id}', [SubCategoryController::class, 'update']);
     Route::get('admin/sub_category/delete/{id}', [SubCategoryController::class, 'delete']);
     
+    Route::get('admin/product/list', [ProductController::class, 'list']);
+    Route::get('admin/product/add', [ProductController::class, 'add']);
+    Route::post('admin/product/add', [ProductController::class, 'insert']);
+    Route::get('admin/product/edit/{id}', [ProductController::class, 'edit']);
+
 });
 
 
